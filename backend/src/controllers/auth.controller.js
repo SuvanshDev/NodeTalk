6c81581e0aa2468759256a3780ce9d4e089a0a64
Suvansh Gupta <suvanshg960@gmail.com>
2026-05-09 01:36:35 +0530

    feat: add user signup endpoint and database connection

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,35 @@
+import User from "../models/User.js";
+
+
+export const signup = async (req,res)=>{
+    const {fullName,email,password} = req.body;
+
+    try{
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        if(password.length<6){
+            return res.status(400).json({message:"Password must be at least 6 characters"});
+        }
+
+        // check if email is valid:regex
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message:"Invalid email address"});
+        }
+
+        const user  = await User.findOne({email:email});
+        if(user){
+            return res.status(400).json({message:"Email already exists"});
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+
+
+
+
+
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import path from "path"
+import { connectDB } from "./lib/db.js";
+import dns from "node:dns/promises";
 
+dns.setServers(["1.1.1.1"]);
+console.log(await dns.getServers());
 
 dotenv.config();
 
@@ -11,6 +15,8 @@ const app = express();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // req.body
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -27,4 +33,7 @@ if(process.env.NODE_ENV === "production"){
 
 
 
-app.listen(PORT, () => console.log("Server running on port:" + PORT));
+app.listen(PORT, () => {
+    console.log("Server running on port:" + PORT)
+    connectDB();
+});
